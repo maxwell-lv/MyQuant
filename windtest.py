6c6ed7fdb4d31005f8ee0e7fd8e03691f0f53ec6
maxwell <maxwell.lv@outlook.com>
2017-01-13 18:57:47 +0800

    init

--- windtest.py
+++ windtest.py
@@ -9,9 +9,10 @@ def DemoWSQCallback(indata):
         print('error code:'+str(indata.ErrorCode)+'\n');
         return();
 
-    print(indata.Fields['RT_TIME'])
+    print(indata.Data[0][0])
 
 if __name__ == "__main__":
     w.start()
 
     data=w.wsq("002337.SZ", "rt_time,rt_pre_close,rt_open,rt_high,rt_low,rt_last,rt_vol,rt_pct_chg,rt_swing,rt_vwap", func=DemoWSQCallback)
+    print(data.Codes)
