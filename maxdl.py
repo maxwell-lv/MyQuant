bb3f817656a0af420462101fed56c5022112e4fe
maxwell.lv <maxwell.lv@outlook.com>
2017-02-08 16:43:31 +0800

    show stock name

--- maxdl.py
+++ maxdl.py
@@ -2,11 +2,13 @@ import click
 import tushare as ts
 import pandas as pd
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+from cn_stock_holidays.zipline.default_calendar import shsz_calendar
 
 from zipline.data.bundles import core as bundles
 
-def nonnew_bundle(environ,
+
+def hs300_bundle(environ,
                  asset_db_writer,
                  minute_bar_writer,
                  daily_bar_writer,
@@ -17,6 +19,80 @@ def nonnew_bundle(environ,
                  cache,
                  show_progress,
                  output_dir):
+    if show_progress:
+        click.echo('getting hs300 stock info')
+
+    hs300 = ts.get_hs300s()
+    get_stock_info(environ,
+                   asset_db_writer,
+                   minute_bar_writer,
+                   daily_bar_writer,
+                   adjustment_writer,
+                   calendar,
+                   start_session,
+                   end_session,
+                   cache,
+                   show_progress,
+                   output_dir,
+                   hs300.code.tolist())
+
+
+def get_stock_info(environ,
+                   asset_db_writer,
+                   minute_bar_writer,
+                   daily_bar_writer,
+                   adjustment_writer,
+                   calendar,
+                   start_session,
+                   end_session,
+                   cache,
+                   show_progress,
+                   output_dir,
+                   symbols):
+
+    ts_symbols = ts.get_stock_basics()
+    assets = []
+    histories = {}
+    i = 0
+    total = len(symbols)
+    for index, row in ts_symbols.iterrows():
+        if index in symbols:
+            i += 1
+            srow = {}
+            click.echo("getting symbol %s(%s) history (%d/%d)" % (index, row['name'], i, total))
+            histories[index] = ts.get_k_data(index, start=timestamp_to_date_string(start_session), end=timestamp_to_date_string(end_session))
+            if histories[index].size == 0:
+                click.echo("no data for %s(%s) between %s and %s" % (index, row['name'], start_session, end_session))
+                continue
+            srow['start_date'] = histories[index].iloc[0].date
+            srow['end_date'] = histories[index].iloc[-1].date
+            srow['symbol'] = index
+            srow['asset_name'] = row['name']
+            assets.append(srow)
+
+    df_symbols = pd.DataFrame(data=assets).sort_values('symbol')
+    symbol_map = pd.DataFrame.copy(df_symbols.symbol)
+
+    # fix the symbol exchange info
+    df = df_symbols.apply(func=convert_symbol_series, axis=1)
+
+    asset_db_writer.write(df_symbols)
+    daily_bar_writer.write(get_hist_data(symbol_map, df, start_session, end_session, calendar),
+                           show_progress=show_progress)
+    adjustment_writer.write()
+
+
+def nonnew_bundle(environ,
+                  asset_db_writer,
+                  minute_bar_writer,
+                  daily_bar_writer,
+                  adjustment_writer,
+                  calendar,
+                  start_session,
+                  end_session,
+                  cache,
+                  show_progress,
+                  output_dir):
     if show_progress:
         click.echo('getting non-new stock info')
 
@@ -58,10 +134,11 @@ def nonnew_bundle(environ,
                            show_progress=show_progress)
     adjustment_writer.write()
 
+
 def timeInttoDate(timeInt):
     return datetime.strptime(str(timeInt), "%Y%m%d").date()
 
-#@bundles.register('maxdl')
+
 def maxdl_bundle(environ,
                  asset_db_writer,
                  minute_bar_writer,
@@ -82,8 +159,10 @@ def maxdl_bundle(environ,
     daily_bar_writer.write(get_hist_data(symbol_map, histories, start_session, end_session, calendar), show_progress=show_progress)
     adjustment_writer.write()
 
+
 def timestamp_to_date_string(timestamp):
     return timestamp.date().strftime('%Y-%m-%d')
+
 
 def get_basic_info(show_progress=True, symbol_list=None, start_session=None, end_session=None):
     if show_progress:
@@ -121,8 +200,8 @@ def get_basic_info(show_progress=True, symbol_list=None, start_session=None, end
     # fix the symbol exchange info
     df = df_symbols.apply(func=convert_symbol_series, axis=1)
 
-
     return df, histories, symbol_map
+
 
 def symbol_to_exchange(symbol):
     isymbol = int(symbol)
@@ -131,11 +210,13 @@ def symbol_to_exchange(symbol):
     else:
         return symbol + ".SZ", "SZSE"
 
+
 def convert_symbol_series(s):
     symbol, e = symbol_to_exchange(s['symbol'])
     s['symbol'] = symbol
     s['exchange'] = e
     return s
+
 
 def get_hist_data(symbol_map, histories, start_session, end_session, calendar):
     for sid, index in symbol_map.iteritems():
@@ -150,11 +231,16 @@ def get_hist_data(symbol_map, histories, start_session, end_session, calendar):
 
 if __name__ == "__main__":
     environ = os.environ
-    sl = environ.get('test_symbols')
-    end = "2016-06-10"
-    start = "2016-06-01"
-    d, h, s = get_basic_info(symbol_list=sl, start_session=start, end_session=end)
-    print(d)
-    for s, h in get_hist_data(s, h):
-        print(s)
-        print(h)
+    end = datetime(year=2016, month=6, day=10)  # "2016-06-10"
+    start = datetime(year=2016, month=6, day=1)  # "2016-06-01"
+    hs300_bundle(environ,
+                 None,
+                 None,
+                 None,
+                 None,
+                 shsz_calendar,
+                 start,
+                 end,
+                 None,
+                 True,
+                 None)
