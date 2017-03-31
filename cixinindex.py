d05b15b6b3e0ba8b6f119dbe95f6872d2742f0fa
maxwell.lv <maxwell.lv@outlook.com>
2017-03-31 16:41:57 +0800

    lianbanindex

--- cixinindex.py
+++ cixinindex.py
@@ -4,6 +4,9 @@ from cn_stock_holidays.zipline.default_calendar import shsz_calendar
 import tushare as ts
 import click
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
 
 #today = datetime.now().date()
 #checkday = today-timedelta(days=1)
@@ -41,14 +44,23 @@ def cxindex(start, end=None):
         data = ts.get_k_data(symbol, end=end.strftime("%Y-%m-%d"), start=test_day.date().strftime("%Y-%m-%d"))
         count = 0
         for row in data.iterrows():
-            print(row)
             if utils.is_yiziban(row[1]):
                 count += 1
-                df.set_value(symbol, row[1]['date'], count)
-                #df.loc[symbol][row[1]['date']] = count
+                column = datetime.strptime(row[1]['date'], "%Y-%m-%d").date()
+                df.set_value(symbol, column, count)
             else:
-                break;
+                break
 
     df.to_csv('test.csv')
 
-cxindex(date(2016, 1, 1))
+
+def calculate_cx_index():
+    df = pd.DataFrame.from_csv('test.csv')
+    result = df.apply(lambda value:value != 0)
+    index = result.sum()
+    index.plot()
+    plt.show()
+
+
+#cxindex(date(2015, 1, 1))
+calculate_cx_index()
